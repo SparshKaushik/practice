bfa01bd7606f172c7c9e8a5a9ef7e1984b961fd3
Sparsh Kaushik <github@sparshkaushik.com>
2022-09-26 05:28:07 +0000

    21

--- cityrain.c
+++ cityrain.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
 
 int main() {
-    float city1, city2, city3;
-    float rainfall[3][7];
-    for (int i = 0; i < 7; i++) {
+    int days;
+    printf("Number of days : ");
+    scanf("%d", &days);
+    float rainfall[3][days];
+    for (int i = 0; i < days; i++) {
         printf("Enter the rainfall for Day %d\n", i + 1);
-        printf("Enter rainfall for city 1: ");
-        scanf("%f", &rainfall[0][i]);
-        printf("Enter rainfall for city 2: ");
-        scanf("%f", &rainfall[1][i]);
-        printf("Enter rainfall for city 3: ");
-        scanf("%f", &rainfall[2][i]);
+        for (int j = 0; j < 3; j++) {
+            printf("City %d : ", j + 1);
+            scanf("%f", &rainfall[j][i]);
+        }
     }
-    for (int i = 0; i < 7; i++) {
+    for (int i = 0; i < days; i++) {
         printf("Day %d: \n", i + 1);
-        printf("Rainfall for city 1: %f\n", rainfall[0][i]);
-        printf("Rainfall for city 2: %f\n", rainfall[1][i]);
-        printf("Rainfall for city 3: %f\n", rainfall[2][i]);
+        for (int j = 0; j < 3; j++) {
+            printf("Rainfall for City %d : %.2f\n", j + 1, rainfall[j][i]);
+        }
     }
+    float variance[3];
+    for (int i = 0; i < 3; i++) {
+        float sum = 0;
+        float variance_sum = 0;
+        for (int j = 0; j < days; j++) {
+            sum += rainfall[i][j];
+            variance_sum += rainfall[i][j] * rainfall[i][j];
+        }
+        printf("Average Rainfall for City %d : %.2f\n", i + 1, sum / days);
+        variance[i] = variance_sum / days - (sum / days) * (sum / days);
+        printf("Variance for City %d : %.2f\n", i + 1, variance[i]);
+    }
+    return 0;
 }
