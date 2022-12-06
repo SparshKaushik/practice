9d1822011db70aa5472c963606a695869ab81eef
Sparsh Kaushik <github@sparshkaushik.com>
2022-12-06 06:18:43 +0000

    woooooooo

---
+++ employee-doubly.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct employee {
+    int id;
+    char name[20];
+    struct employee *next;
+    struct employee *prev;
+};
+
+struct employee *head = NULL;
+struct employee *tail = NULL;
+
+void insert(int id, char name[]) {
+    struct employee *new = (struct employee *)malloc(sizeof(struct employee));
+    new->id = id;
+    strcpy(new->name, name);
+    new->next = NULL;
+    new->prev = NULL;
+
+    if (head == NULL) {
+        head = new;
+        tail = new;
+    } else {
+        head->prev = new;
+        new->next = head;
+        head = new;
+    }
+}
+
+void delete() {
+    if (head == NULL) {
+        printf("List is empty");
+    } else {
+        struct employee *temp = tail;
+        tail = tail->prev;
+        tail->next = NULL;
+        free(temp);
+    }
+}
+
+void display() {
+    struct employee *temp = head;
+    while (temp != NULL) {
+        printf("Employee #%d\n%s", temp->id, temp->name);
+        temp = temp->next;
+    }
+}
+
+int main(void) {
+    int op;
+    while (op != 4) {
+        printf("\n1. Insert \n2. Delete \n3. Display \n4. Exit \nSelect an option: ");
+        scanf("%d", &op);
+        switch (op) {
+        case 1:
+            printf("Enter Id: ");
+            int id;
+            scanf("%d", &id);
+            printf("Enter Name: ");
+            char name[20];
+            scanf("%s", name);
+            insert(id, name);
+            break;
+        case 2:
+            delete();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            printf("Exiting...");
+            exit(0);
+        default:
+            printf("Invalid option");
+        }
+    }
+
+    return 0;
+}
